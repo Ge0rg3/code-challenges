2266c3b0ed8d3edd0c8cd34a975507380ed1022b
George O <georgeomnet@gmail.com>
2018-10-17 14:41:56 +0100

    Update 6 | Dubstep.py

--- codewars/python/6 | Dubstep.py
+++ codewars/python/6 | Dubstep.py
@@ -3,5 +3,4 @@
 #The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
 #Return the words of the initial song that Polycarpus used to make a dubsteb remix. Separate the words with a space.
 #e.g. songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")  -->  WE ARE THE CHAMPIONS MY FRIEND.
-def song_decoder(song):
-    return (song.replace('WUB',' ').replace('   ',' ')).replace('  ',' ').strip()
+song_decoder = lambda song : (song.replace('WUB',' ').replace('   ',' ')).replace('  ',' ').strip()
