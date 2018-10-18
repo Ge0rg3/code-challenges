c67881bd2dc024b2e2f88bb2f57a601f7988f698
George O <georgeomnet@gmail.com>
2018-10-18 09:55:45 +0100

    Update 6 | Find the Unique Number.py

--- codewars/python/6 | Find the Unique Number.py
+++ codewars/python/6 | Find the Unique Number.py
@@ -5,3 +5,7 @@ def find_uniq(arr):
     arr.sort()
     return arr[0] if arr.count(arr[0]) == 1 else arr[-1]
     
+"""
+Here is a one-liner that is *far* slower:
+find_uniq = lambda arr : [i for i in arr if arr.count(i) == 1][0]
+"""
