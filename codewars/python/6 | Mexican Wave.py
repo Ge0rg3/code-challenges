9934b47f1f995956931f8ef64916bab0d157531c
George O <georgeomnet@gmail.com>
2018-10-18 09:42:57 +0100

    Update 6 | Mexican Wave.py

--- codewars/python/6 | Mexican Wave.py
+++ codewars/python/6 | Mexican Wave.py
@@ -2,9 +2,4 @@
 #1.  The input string will always be lower case but maybe empty.
 #2.  If the character in the string is whitespace then pass over it as if it was an empty seat.
 #wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
-def wave(str):
-    arr = []
-    for c, i in enumerate(str):
-        if i != " ":
-            arr.append(str[:c]+str[c].upper()+str[c+1:])
-    return arr
+wave = lambda str : [str[:n]+str[n].upper()+str[n+1:] for n in range(0, len(str)) if str[n] != " "]
