fc2d9b65958cbdc9923b61018cda12000c22df55
George O <georgeomnet@gmail.com>
2018-10-18 09:18:08 +0100

    Update 7 | Disemvowel Trolls.py

--- codewars/python/7 | Disemvowel Trolls.py
+++ codewars/python/7 | Disemvowel Trolls.py
@@ -1,9 +1,3 @@
 #Your task is to write a function that takes a string and return a new string with all vowels removed.
 #For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-vowels = ["a","e","i","o","u"]
-def disemvowel(string):
-    nstring = ""
-    for i in string:
-        if i.lower() not in vowels:
-            nstring += (str(i))
-    return nstring
+disemvowel = lambda s : ''.join(i for i in s if i.lower() not in "aeiou")
